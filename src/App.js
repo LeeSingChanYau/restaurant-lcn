7f2ce089eaa8efe6229a6d4668e10dd9950ebea3
lee sing chan <leesingchan@lees-MacBook-Air.local>
2021-05-24 19:30:13 -0400

    Used material-ui: created the navbar with the AppBar component and changed the background color to a dark green

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  abRoot: {
+    backgroundColor: "#1f9c41",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -23,14 +29,16 @@ function App() {
   const classes = useStyles();
 
   return (
-    <div>
-      <AppBar position="static">
+    <div className={classes.root}> 
+      <AppBar position="static" classes={{ 
+      root: classes.abRoot,
+      }}>
       <Toolbar>
         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
         <Typography variant="h6" className={classes.title}>
-          News
+          Restaurant La Ciudad Nueva
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
