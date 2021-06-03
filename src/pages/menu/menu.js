8c89a1fbf5bf1a95a293692a4681b6cf1cb1a8c6
lee sing chan <leesingchan@lees-MacBook-Air.local>
2021-06-03 15:39:02 -0400

    Worked on menu component. Added the menu photos and some text.

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './menu.css';
+import Entradas from '../../images/entradas.jpeg';
+import Platos from '../../images/platos.jpeg';
+import Especialidades from '../../images/especialidades.jpeg';
+import Menus from '../../images/menus.jpeg';
 
 const Menu = () => (
     <div>
-        <h1 className="title">
-            Menu
-        </h1>
-        <img/>
-        <img/>
-        <img/>
+        <h1 className="title">  Menu </h1>
+        <hr className="line"></hr>
+        <h2 className="title"> Entradas </h2>
+        <img src={Entradas} className="menu" alt="entradas.jpeg"/>
+        <h2 className="title"> Verduras y Carnes </h2>
+        <img src={Platos} className="menu" alt="platos.jpeg"/>
+        <h2 className="title"> Especialidades </h2>
+        <img src={Especialidades} className="menu" alt="especialidades.jpeg"/>
+        <h2 className="title"> Menus </h2>
+        <img src={Menus} className="menu" alt="menus.jpeg"/>
     </div>
 )
 
